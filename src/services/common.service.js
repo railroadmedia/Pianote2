b7dfac2f2b34df50e150e2431e4d299a0ffeb8c6
kp98 <kp98@Kentons-Mac-mini.local>
2020-11-20 16:55:27 -0800

    commenting

--- src/services/common.service.js
+++ src/services/common.service.js
@@ -1,6 +1,7 @@
 export default {
-  rootUrl: 'https://www.pianote.com',
+  rootUrl: 'https://staging.pianote.com',
   //'https://www.pianote.com',
+  //'https://staging.pianote.com'
   tryCall: async function (url, method, body) {
     try {
       if (body) body = body ? JSON.stringify(body) : null;
