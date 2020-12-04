65b8fb371434c9dac8052f1e8739184ea4ee00b9
Bogdan <>
2020-12-04 14:04:12 +0200

    * fixed student focus cache destination

--- src/redux/StudentFocusCacheActions.js
+++ src/redux/StudentFocusCacheActions.js
@@ -2,7 +2,7 @@ import RNFetchBlob from 'rn-fetch-blob';
 
 export const cacheStudentFocus = studentFocus => {
   RNFetchBlob.fs.writeFile(
-    `${RNFetchBlob.fs.dirs.DocumentDir}/cacheSongs`,
+    `${RNFetchBlob.fs.dirs.DocumentDir}/cacheStudentFocus`,
     JSON.stringify(studentFocus),
     'utf8'
   );
