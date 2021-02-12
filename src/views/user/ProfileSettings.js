295cf27b3d645e64b1882d88e1f656d8788be290
Bogdan <>
2021-02-12 18:41:51 +0200

    * handle big image uploads * added loading screen on avatar change

--- src/views/user/ProfileSettings.js
+++ src/views/user/ProfileSettings.js
@@ -31,6 +31,7 @@ import ProfileImage from '../../modals/ProfileImage.js';
 import NavigationBar from '../../components/NavigationBar.js';
 import commonService from '../../services/common.service.js';
 import { NetworkContext } from '../../context/NetworkProvider.js';
+import Loading from '../../components/Loading.js';
 
 const windowDim = Dimensions.get('window');
 const width =
@@ -77,14 +78,16 @@ export default class ProfileSettings extends React.Component {
     });
   };
 
-  save() {
+  async save() {
+    this.loadingRef?.toggleLoading();
     if (this.state.currentlyView == 'Display Name') {
-      this.changeName();
+      await this.changeName();
     } else if (this.state.currentlyView == 'Profile Photo') {
-      this.changeImage();
+      await this.changeImage();
     } else if (this.state.currentlyView == 'Password') {
-      this.changePassword();
+      await this.changePassword();
     }
+    this.loadingRef?.toggleLoading();
   }
 
   changePassword = async () => {
@@ -116,11 +119,7 @@ export default class ProfileSettings extends React.Component {
           display_name: this.state.displayName
         }
       );
-
-      console.log(nameResponse);
-
       await AsyncStorage.setItem('displayName', this.state.displayName);
-
       this.props.navigation.dispatch(resetAction);
     } else {
       this.setState({ showDisplayName: true });
@@ -140,9 +139,6 @@ export default class ProfileSettings extends React.Component {
       uri: this.state.imageURI
     });
     data.append('target', this.state.imageName);
-
-    console.log(data);
-
     try {
       if (this.state.imageURI !== '') {
         let response = await fetch(
@@ -153,13 +149,11 @@ export default class ProfileSettings extends React.Component {
             body: data
           }
         );
-        console.log('URL: ', response);
         if (response.status == 413) {
           this.setState({ showProfileImage: true });
           return;
         }
         let url = await response.json();
-
         if (url.data[0].url) {
           await commonService.tryCall(
             `${commonService.rootUrl}/api/profile/update`,
@@ -173,9 +167,7 @@ export default class ProfileSettings extends React.Component {
           this.props.navigation.dispatch(resetAction);
         }
       }
-    } catch (error) {
-      console.log('ERROR UPLOADING IMAGE: ', error);
-    }
+    } catch (error) {}
   };
 
   chooseImage = async () => {
@@ -185,7 +177,8 @@ export default class ProfileSettings extends React.Component {
         storageOptions: {
           skipBackup: true,
           path: 'images'
-        }
+        },
+        maxWidth: 1000
       },
       response => {
         if (response.didCancel) {
@@ -204,215 +197,237 @@ export default class ProfileSettings extends React.Component {
 
   render() {
     return (
-      <SafeAreaView style={styles.mainContainer}>
-        <StatusBar
-          backgroundColor={colors.mainBackground}
-          barStyle={'light-content'}
-        />
-        <View style={localStyles.myProfileSettings}>
-          <TouchableOpacity
-            onPress={() => {
-              this.state.currentlyView == 'Profile Settings'
-                ? this.props.navigation.goBack()
-                : this.setState({
-                    currentlyView: 'Profile Settings'
-                  });
-            }}
-            style={{ flex: 1 }}
-          >
-            <Back
-              width={(onTablet ? 17.5 : 25) * factor}
-              height={(onTablet ? 17.5 : 25) * factor}
-              fill={colors.secondBackground}
-            />
-          </TouchableOpacity>
-          <Text
-            style={[styles.childHeaderText, { color: colors.secondBackground }]}
-          >
-            {this.state.currentlyView}
-          </Text>
-          {this.state.currentlyView !== 'Profile Settings' ? (
-            <TouchableOpacity
-              onPress={() => {
-                this.save();
-              }}
-              style={{ flex: 1 }}
-            >
-              <Text style={localStyles.save}>SAVE</Text>
-            </TouchableOpacity>
-          ) : (
+      <>
+        <SafeAreaView style={styles.mainContainer}>
+          <StatusBar
+            backgroundColor={colors.mainBackground}
+            barStyle={'light-content'}
+          />
+          <View style={localStyles.myProfileSettings}>
             <View style={{ flex: 1 }} />
-          )}
-        </View>
-
-        {this.state.currentlyView == 'Profile Settings' && (
-          <ScrollView style={{ flex: 1 }}>
-            <TouchableOpacity
-              style={[styles.centerContent, localStyles.displayContainer]}
-              onPress={() =>
-                this.setState({
-                  currentlyView: 'Display Name'
-                })
-              }
+            <Text
+              style={[
+                styles.childHeaderText,
+                { color: colors.secondBackground }
+              ]}
             >
-              <Text style={styles.settingsText}>Display Name</Text>
+              {this.state.currentlyView}
+            </Text>
+            {this.state.currentlyView !== 'Profile Settings' ? (
+              <TouchableOpacity
+                onPress={() => {
+                  this.save();
+                }}
+                style={{ flex: 1 }}
+              >
+                <Text style={localStyles.save}>SAVE</Text>
+              </TouchableOpacity>
+            ) : (
               <View style={{ flex: 1 }} />
-              <AntIcon
-                name={'right'}
-                size={(onTablet ? 20 : 25) * factor}
-                color={colors.secondBackground}
+            )}
+          </View>
+
+          {this.state.currentlyView == 'Profile Settings' && (
+            <ScrollView style={{ flex: 1 }}>
+              <TouchableOpacity
+                style={[styles.centerContent, localStyles.displayContainer]}
+                onPress={() =>
+                  this.setState({
+                    currentlyView: 'Display Name'
+                  })
+                }
+              >
+                <Text style={styles.settingsText}>Display Name</Text>
+                <View style={{ flex: 1 }} />
+                <AntIcon
+                  name={'right'}
+                  size={(onTablet ? 20 : 25) * factor}
+                  color={colors.secondBackground}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.centerContent, localStyles.profilePhoto]}
+                onPress={() => {
+                  this.setState({
+                    currentlyView: 'Profile Photo'
+                  });
+                }}
+              >
+                <Text style={styles.settingsText}>Profile Photo</Text>
+                <View style={{ flex: 1 }} />
+                <AntIcon
+                  name={'right'}
+                  size={(onTablet ? 20 : 25) * factor}
+                  color={colors.secondBackground}
+                />
+              </TouchableOpacity>
+            </ScrollView>
+          )}
+          {this.state.currentlyView == 'Display Name' && (
+            <ScrollView style={styles.mainContainer}>
+              <TextInput
+                ref={txt => {
+                  this.txt = txt;
+                }}
+                placeholder={'Display Name'}
+                value={this.state.displayName}
+                placeholderTextColor={colors.secondBackground}
+                onChangeText={displayName => this.setState({ displayName })}
+                onSubmitEditing={() => {}}
+                returnKeyType={'go'}
+                style={[
+                  styles.centerContent,
+                  localStyles.displayContainer,
+                  localStyles.textInput
+                ]}
               />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.centerContent, localStyles.profilePhoto]}
-              onPress={() => {
+              <Text style={localStyles.text}>
+                This is the name that will appear on your comments and forum
+                posts.
+              </Text>
+            </ScrollView>
+          )}
+          {this.state.currentlyView == 'Profile Photo' && (
+            <ScrollView style={{ flex: 1 }}>
+              <View style={[localStyles.scrollContainer, styles.centerContent]}>
+                {this.state.imageURI !== '' && (
+                  <View style={{ flex: 1 }}>
+                    <FastImage
+                      style={localStyles.image}
+                      source={{ uri: this.state.imageURI }}
+                      resizeMode={FastImage.resizeMode.cover}
+                    />
+                    <TouchableOpacity
+                      style={{
+                        ...localStyles.crossContainer,
+                        right: -((onTablet ? 22.5 : 30) * factor + 10 + 4)
+                      }}
+                      onPress={() =>
+                        this.setState({
+                          imageURI: '',
+                          imageType: '',
+                          imageName: ''
+                        })
+                      }
+                    >
+                      <EntypoIcon
+                        name={'cross'}
+                        size={(onTablet ? 22.5 : 30) * factor}
+                        color={colors.secondBackground}
+                        style={{
+                          width: (onTablet ? 22.5 : 30) * factor,
+                          height: (onTablet ? 22.5 : 30) * factor
+                        }}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                )}
+
+                {this.state.imageURI == '' && (
+                  <TouchableOpacity
+                    onPress={() => this.chooseImage()}
+                    style={styles.centerContent}
+                  >
+                    <AntIcon name={'plus'} size={65 * factor} color={'white'} />
+                  </TouchableOpacity>
+                )}
+              </View>
+              <Text style={localStyles.imageText}>
+                This is the image that will appear with your comments and forum
+                posts.
+              </Text>
+
+              <TouchableOpacity
+                onPress={() => this.chooseImage()}
+                style={[styles.centerContent, localStyles.imageContainer]}
+              >
+                <IonIcon
+                  size={(onTablet ? 40 : 50) * factor}
+                  name={'ios-camera'}
+                  color={colors.secondBackground}
+                  style={{ padding: 10 * factor }}
+                />
+              </TouchableOpacity>
+            </ScrollView>
+          )}
+
+          {this.state.currentlyView == 'Profile Settings' && (
+            <NavigationBar currentPage={'PROFILE'} pad={true} />
+          )}
+
+          <Modal
+            isVisible={this.state.showDisplayName}
+            style={[
+              styles.centerContent,
+              {
+                margin: 0,
+                flex: 1
+              }
+            ]}
+            animation={'slideInUp'}
+            animationInTiming={350}
+            animationOutTiming={350}
+            coverScreen={true}
+            hasBackdrop={true}
+          >
+            <DisplayName
+              hideDisplayName={() => {
                 this.setState({
-                  currentlyView: 'Profile Photo'
+                  showDisplayName: false
                 });
               }}
-            >
-              <Text style={styles.settingsText}>Profile Photo</Text>
-              <View style={{ flex: 1 }} />
-              <AntIcon
-                name={'right'}
-                size={(onTablet ? 20 : 25) * factor}
-                color={colors.secondBackground}
-              />
-            </TouchableOpacity>
-          </ScrollView>
-        )}
-        {this.state.currentlyView == 'Display Name' && (
-          <ScrollView style={styles.mainContainer}>
-            <TextInput
-              ref={txt => {
-                this.txt = txt;
-              }}
-              placeholder={'Display Name'}
-              value={this.state.displayName}
-              placeholderTextColor={colors.secondBackground}
-              onChangeText={displayName => this.setState({ displayName })}
-              onSubmitEditing={() => {}}
-              returnKeyType={'go'}
-              style={[
-                styles.centerContent,
-                localStyles.displayContainer,
-                localStyles.textInput
-              ]}
             />
-            <Text style={localStyles.text}>
-              This is the name that will appear on your comments and forum
-              posts.
-            </Text>
-          </ScrollView>
-        )}
-        {this.state.currentlyView == 'Profile Photo' && (
-          <ScrollView style={{ flex: 1 }}>
-            <View style={[localStyles.scrollContainer, styles.centerContent]}>
-              {this.state.imageURI !== '' && (
-                <View style={{ flex: 1 }}>
-                  <FastImage
-                    style={localStyles.image}
-                    source={{ uri: this.state.imageURI }}
-                    resizeMode={FastImage.resizeMode.cover}
-                  />
-                  <TouchableOpacity
-                    style={localStyles.crossContainer}
-                    onPress={() =>
-                      this.setState({
-                        imageURI: '',
-                        imageType: '',
-                        imageName: ''
-                      })
-                    }
-                  >
-                    <EntypoIcon
-                      name={'cross'}
-                      size={(onTablet ? 22.5 : 30) * factor}
-                      color={colors.secondBackground}
-                    />
-                  </TouchableOpacity>
-                </View>
-              )}
-
-              {this.state.imageURI == '' && (
-                <TouchableOpacity
-                  onPress={() => this.chooseImage()}
-                  style={styles.centerContent}
-                >
-                  <AntIcon name={'plus'} size={65 * factor} color={'white'} />
-                </TouchableOpacity>
-              )}
-            </View>
-            <Text style={localStyles.imageText}>
-              This is the image that will appear with your comments and forum
-              posts.
-            </Text>
-
+          </Modal>
+          <Modal
+            isVisible={this.state.showProfileImage}
+            style={[
+              styles.centerContent,
+              {
+                margin: 0,
+                flex: 1
+              }
+            ]}
+            animation={'slideInUp'}
+            animationInTiming={350}
+            animationOutTiming={350}
+            coverScreen={true}
+            hasBackdrop={true}
+          >
+            <ProfileImage
+              hideProfileImage={() => {
+                this.setState({
+                  showProfileImage: false
+                });
+              }}
+            />
+          </Modal>
+        </SafeAreaView>
+        <Loading ref={ref => (this.loadingRef = ref)} />
+        <SafeAreaView style={{ position: 'absolute', zIndex: 3 }}>
+          <View>
             <TouchableOpacity
-              onPress={() => this.chooseImage()}
-              style={[styles.centerContent, localStyles.imageContainer]}
+              onPress={() => {
+                this.loadingRef?.toggleLoading(false);
+                this.state.currentlyView == 'Profile Settings'
+                  ? this.props.navigation.goBack()
+                  : this.setState({
+                      currentlyView: 'Profile Settings'
+                    });
+              }}
+              style={{
+                position: 'absolute',
+                padding: 15
+              }}
             >
-              <IonIcon
-                size={(onTablet ? 40 : 50) * factor}
-                name={'ios-camera'}
-                color={colors.secondBackground}
-                style={{ padding: 10 * factor }}
+              <Back
+                width={(onTablet ? 17.5 : 25) * factor}
+                height={(onTablet ? 17.5 : 25) * factor}
+                fill={colors.secondBackground}
               />
             </TouchableOpacity>
-          </ScrollView>
-        )}
-
-        {this.state.currentlyView == 'Profile Settings' && (
-          <NavigationBar currentPage={'PROFILE'} pad={true} />
-        )}
-
-        <Modal
-          isVisible={this.state.showDisplayName}
-          style={[
-            styles.centerContent,
-            {
-              margin: 0,
-              flex: 1
-            }
-          ]}
-          animation={'slideInUp'}
-          animationInTiming={350}
-          animationOutTiming={350}
-          coverScreen={true}
-          hasBackdrop={true}
-        >
-          <DisplayName
-            hideDisplayName={() => {
-              this.setState({
-                showDisplayName: false
-              });
-            }}
-          />
-        </Modal>
-        <Modal
-          isVisible={this.state.showProfileImage}
-          style={[
-            styles.centerContent,
-            {
-              margin: 0,
-              flex: 1
-            }
-          ]}
-          animation={'slideInUp'}
-          animationInTiming={350}
-          animationOutTiming={350}
-          coverScreen={true}
-          hasBackdrop={true}
-        >
-          <ProfileImage
-            hideProfileImage={() => {
-              this.setState({
-                showProfileImage: false
-              });
-            }}
-          />
-        </Modal>
-      </SafeAreaView>
+          </View>
+        </SafeAreaView>
+      </>
     );
   }
 }
@@ -481,18 +496,11 @@ const localStyles = StyleSheet.create({
     marginTop: 15 * factor
   },
   crossContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'flex-end',
     position: 'absolute',
-    height: (DeviceInfo.isTablet() ? 30 : 40) * factor,
-    width: (DeviceInfo.isTablet() ? 30 : 40) * factor,
-    top: (DeviceInfo.isTablet() ? -7.5 : -10) * factor,
-    right: (DeviceInfo.isTablet() ? -7.5 : -10) * factor,
-    borderRadius: 100,
+    padding: 5,
     borderColor: '#445f73',
     borderWidth: 2,
-    marginTop: 10 * factor
+    borderRadius: 100
   },
   imageText: {
     fontFamily: 'OpenSans-Regular',
