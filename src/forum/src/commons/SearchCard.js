e162ca60a49cb1e702361f7ff6e6b473c89003b1
ARTSOFT\csillaj <jenei_csilla@yahoo.com>
2021-06-09 16:00:51 +0300

    * added searchcard component

--- src/forum/src/commons/SearchCard.js
+++ src/forum/src/commons/SearchCard.js
@@ -1,8 +1,106 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { arrowRight } from '../assets/svgs';
+import AccessLevelAvatar from './AccessLevelAvatar';
+import HTMLRenderer from './HTMLRenderer';
+
+let styles;
 
 export default class SearchCard extends React.Component {
+  constructor(props) {
+    super(props);
+    let { isDark } = props;
+    styles = setStyles(isDark);
+  }
+
   render() {
-    return <View></View>;
+    console.log(this.props.item);
+    const {
+      item: {
+        content,
+        thread: {
+          author_avatar_url,
+          author_access_level,
+          author_display_name,
+          title,
+          published_on_formatted,
+          post_count,
+          latest_post,
+          category
+        }
+      },
+      isDark,
+      appColor
+    } = this.props;
+
+    return (
+      <TouchableOpacity style={styles.container}>
+        <View style={styles.centerWithSpacing}>
+          <View style={styles.center}>
+            <AccessLevelAvatar
+              author={{
+                avatar_url: author_avatar_url,
+                access_level: author_access_level
+              }}
+              height={45}
+              appColor={appColor}
+              isDark={isDark}
+              tagHeight={4}
+            />
+            <View style={{ marginLeft: 10 }}>
+              <Text style={styles.title}>{title}</Text>
+              <Text style={styles.text}>
+                Started on {published_on_formatted} by {author_display_name}
+              </Text>
+            </View>
+          </View>
+          <Text style={styles.text}>{post_count} Replies</Text>
+        </View>
+        <View style={styles.centerWithSpacing}>
+          <Text style={{ color: '#FFFFFF' }}>
+            {content.replace(/<[^>]*>?/gm, '').slice(0, 200) + ' ...'}
+          </Text>
+
+          {arrowRight({ height: 15, fill: isDark ? 'white' : 'black' })}
+        </View>
+        <Text style={styles.text}>
+          Replied {latest_post.created_at_diff} by{' '}
+          {latest_post.author_display_name} - {category}
+        </Text>
+      </TouchableOpacity>
+    );
   }
 }
+
+let setStyles = isDark =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: isDark ? '#081825' : 'white',
+      padding: 10,
+      marginBottom: 15
+    },
+    title: {
+      fontFamily: 'OpenSans',
+      color: isDark ? 'white' : 'black',
+      fontSize: 20,
+      fontWeight: '700'
+    },
+    text: {
+      fontFamily: 'OpenSans',
+      fontWeight: '100',
+      color: '#445F74',
+      fontSize: 14,
+      paddingVertical: 5
+    },
+    centerWithSpacing: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flex: 1
+    },
+    center: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center'
+    }
+  });
